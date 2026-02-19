9ecac2d8fffa8ae4ef20dce4c554fe196050a5df
Omer Can Durmus <omercandurmuss@gmail.com>
2026-02-19 15:53:18 -0600

    pp and regulated pp algorithms compared

--- simulink_models/path_visualization.m
+++ simulink_models/path_visualization.m
@@ -1,5 +1,7 @@
 plot(xRef,yRef);
 hold on;
 plot(out.tracked_path_x,out.tracked_path_y);
-legend("ref","tracked path");
+hold on
+plot(out.tracked_path_x2,out.tracked_path_y2);
+legend("ref","regulated pp path","pp");
 
